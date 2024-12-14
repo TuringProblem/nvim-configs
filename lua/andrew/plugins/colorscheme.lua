cab2b5105757752d99bc52adaadc0b0b4b71e347
Taziz <122590935+TuringProblem@users.noreply.github.com>
2024-12-14 13:58:07 -0500

    feat: Added another color called 'Military' theme :o to implement go to configs, and search for the file colorscheme.lua -> inside here is where you are going to change the theme from 'mytheme' -> 'military' (should already be set by default on this commit

--- lua/andrew/plugins/colorscheme.lua
+++ lua/andrew/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
   {
     dir = "~/.config/nvim/lua/colors", -- Correct path to your custom theme directory
-    name = "mytheme",
+    name = "military",
     config = function()
-      require("colors.mytheme").setup()
+      require("colors.military").setup()
       vim.cmd("colorscheme mytheme")
     end,
   },
