479afa0621c24444c151a098d542269eaaab05b6
Override <122590935+TuringProblem@users.noreply.github.com>
2025-03-07 16:44:04 -0500

    feat: started to add a floating terminal - shoutout to teejdevris youtube video, so thank you

--- lua/andrew/core/keymaps.lua
+++ lua/andrew/core/keymaps.lua
@@ -2,18 +2,22 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
-
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- Increment/Decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- Increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- Decrement
 
+-- looking up keymappings real quick so this just gonna be boring for you rn.. :/ one sec
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "[S]plit window [V]ertically" }) -- show window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "[S]plit window [H]orizontally" }) -- show window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "[S]plits size [E]qually" }) -- makes split windows equal width
 keymap.set("n", "<leader>sc", "<cmd>close<CR>", { desc = "[S]plit window [C]lose" }) -- closes the current split window
+
+-- Switch between windows
+keymap.set("n", "<leader>vn", "<cmd>windo wincmd k<CR>", { desc = " [V]iew [N]ext" })
+keymap.set("n", "<leader>vb", "<cmd>windo wincmd h<CR>", { desc = " [V]iew [B]ack" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "[T]ab [O]pen (new)" }) -- opens a new tab
 keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "[T]ab [C]lose" }) -- closes current tab
@@ -22,5 +26,5 @@ keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "[T]left ab [P]revious <
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --
 
 -- Indentation
-keymap.set("v", "<", "<gv", { desc = "Indent left in visual mode."})
-keymap.set("v", ">", ">gv", { desc = "Indent right in visual mode."})
+keymap.set("v", "<", "<gv", { desc = "Indent left in visual mode." })
+keymap.set("v", ">", ">gv", { desc = "Indent right in visual mode." })
