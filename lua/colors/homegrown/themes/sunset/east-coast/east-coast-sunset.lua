d811fc1a57e3b406945d47efe429bf31ff8c56d1
Override <122590935+TuringProblem@users.noreply.github.com>
2026-06-07 00:12:55 -0400

    feat: colors

---
+++ lua/colors/homegrown/themes/sunset/east-coast/east-coast-sunset.lua
@@ -0,0 +1,21 @@
+local c = require("colors.homegrown.palette")
+local f = vim.api.nvim_set_hl
+local theme = {}
+
+theme.set_highlights = function()
+  f(0, "Normal", { fg = c.white.azure_white, bg = c.black.dim_gray })
+  f(0, "SignColumn", { fg = c.purple[1].byzantium, bg = c.black.dim_gray })
+  f(0, "Comment", { fg = c.purple[2].dark_purple, bg = c.white.almond })
+end
+
+function theme.setup()
+  vim.cmd("highlight clear")
+  if vim.fn.exists("syntax_on") then
+    vim.cmd("syntax reset")
+  end
+
+  vim.g.colors_name = "east-coast-sunset"
+  theme.set_highlights()
+end
+
+return theme
